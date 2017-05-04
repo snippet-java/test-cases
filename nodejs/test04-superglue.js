af5279d8e3acb5ffd3e736eee29b6361007230cf
snippet-java <snippet.java@gmail.com>
2017-05-04 17:10:56 +0800

    update

--- nodejs/test04-superglue.js
+++ nodejs/test04-superglue.js
@@ -3,6 +3,7 @@ var parameters = {
     "text2": "hello world 2",
     "text3": "hello world 3"
 }
+const templateUrl = "https://gist.githubusercontent.com/snippet-java/fd3aa1c2ab893bf8e1bcd90073ceab99/raw/d3bf1ddae4ca29b3ba1f8d4e851491f5869145dd/superglue.html";
 
 function main(params) {
     if (params.__ow_method == "get") {
@@ -11,24 +12,11 @@ function main(params) {
             inputs += "<input name='" + i + "' placeholder='" + i + "' value='" + parameters[i] + "' style='width:100%; padding:10px'><br/>"
         }
 
-        return {
-            html: "\
-        <html><head>\
-        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>\
-        <script>\n\
-        $(function() {\n\
-            var url = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);\n\
-            url = url.substr(0, url.indexOf('.')) + '.json';\n\
-            $('#submit').click(function() {\n\
-                var data = {};\n\
-                $('input').each(function(i, el) { data[$(this).attr('name')] = $(this).val(); })\n\
-                $.ajax({ url, data, method: 'POST' })\n\
-                .done(function(data) { $('#result').val(JSON.stringify(data,null,2)); });\n\
-            })\n\
-        })\n\
-        </script></head><body>" + inputs + "<button id='submit' style='width: 100%; padding: 10px;'>Submit</button>\
-        <textarea id='result' style='width: 100%; height: 500px'></textarea></body></html>"
-        }
+        return new Promise(function (resolve, reject) {
+            request(templateUrl, function(err, res, body) {
+                return resolve({html:body.replace("{{inputs}}", inputs)});
+            })
+        });
     }
 
     return new Promise(function (resolve, reject) {
