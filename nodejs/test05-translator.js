3672214c20e6d0313c22ccd49bd4442114096395
snippet-java <snippet.java@gmail.com>
2017-05-04 17:40:06 +0800

    update

--- nodejs/test05-translator.js
+++ nodejs/test05-translator.js
@@ -6,6 +6,8 @@ var parameters = {
     "password": "",
     "url": 'https://gateway.watsonplatform.net/language-translator/api'
 }
+const request = require("request");
+const templateUrl = "https://gist.githubusercontent.com/snippet-java/fd3aa1c2ab893bf8e1bcd90073ceab99/raw";
 
 function main(params) {
     if (params.__ow_method == "get") {
@@ -13,25 +15,12 @@ function main(params) {
         for (i in parameters) {
             inputs += "<input name='" + i + "' placeholder='" + i + "' value='" + parameters[i] + "' style='width:100%; padding:10px'><br/>"
         }
-
-        return {
-            html: "\
-        <html><head>\
-        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>\
-        <script>\n\
-        $(function() {\n\
-            var url = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);\n\
-            url = url.substr(0, url.indexOf('.')) + '.json';\n\
-            $('#submit').click(function() {\n\
-                var data = {};\n\
-                $('input').each(function(i, el) { data[$(this).attr('name')] = $(this).val(); })\n\
-                $.ajax({ url, data, method: 'POST' })\n\
-                .done(function(data) { $('#result').val(JSON.stringify(data,null,2)); });\n\
-            })\n\
-        })\n\
-        </script></head><body>" + inputs + "<button id='submit' style='width: 100%; padding: 10px;'>Submit</button>\
-        <textarea id='result' style='width: 100%; height: 500px'></textarea></body></html>"
-        }
+        
+        return new Promise(function (resolve, reject) {
+            request(templateUrl, function(err, res, body) {
+                return resolve({html:body.replace("{{inputs}}", inputs)});
+            })
+        });
     }
 
     return new Promise(function (resolve, reject) {
